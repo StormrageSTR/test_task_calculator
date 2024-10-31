4975c6312a2dcb036945773358e79bd2bffec5af
AlexanderM <darkness174rus@gmail.com>
2024-10-31 12:36:16 +0500

    Тесты на то что калькулятор считает корректно

--- CalculatorTest/UnitTest1.cs
+++ CalculatorTest/UnitTest1.cs
@@ -5,8 +5,10 @@ namespace CalculatorTest;
 public class UnitTest1
 {
     [Theory]
-    [InlineData("1+2-3", 0)]
-    public void ExpressionEvaluatedTest(string expression, int expectedResult)
+    [InlineData("1 + 2 - 3", 0)]
+    [InlineData("10 + (4 * 3 + (5 / 2))", 24.5)]
+    [InlineData("(7 * 2) + 25 - (84 / 3)", 11)]
+    public void ExpressionEvaluatedTest(string expression, double expectedResult)
     {
         Assert.Equal(expectedResult, Calculator.StartEvaluation(expression));
     }
